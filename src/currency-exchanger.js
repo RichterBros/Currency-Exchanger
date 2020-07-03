ff19a88d0201d6e1bf5cac20bb3fdb4d6f4ceef4
Erich Richter <erich.richter@gmail.com>
2020-07-03 11:28:17 -0700

    add api key, exchange object

--- src/currency-exchanger.js
+++ src/currency-exchanger.js
@@ -1,2 +1,12 @@
 // project business logic goes here
 // exports!
+
+export class Exchange {
+  constructor(USD, AED, ARS, AUD, BGN) {
+    this.USD = USD;
+    this.AED = AED;
+    this.ARS = ARS;
+    this.AUD = AUD;
+    this.BGN = BGN;
+  }
+}
--- src/main.js
+++ src/main.js
@@ -2,10 +2,47 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { Exchange } from './currency-exchanger';
 
-$(document).ready(function () {
-  // $('#submit-button').submit(function (event) {
-  //     event.preventDefault();
+function getElements(response) {
+  
+  let exchange;
+  
+  if (response) {
 
-  // });
+    exchange = new Exchange(response.conversion_rates);
+    //dino.splitDino();
+
+   
+
+  } else {
+    $('#dino-output').text("");
+    $('#error-output').text(`There was an error: ${response.message}`);
+  }
+}
+
+
+
+
+
+$(document).ready(function() {
+  //$('#generate').click(function() {
+
+    $('.keyboard').remove();
+    $('span').remove();
+
+    fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      })
+      .then(function(jsonifiedResponse) {
+        getElements(jsonifiedResponse);
+      //});
+  });
 });
