17f07b588322ea4ae35e8a90b6e016c2a81d0fbf
Erich Richter <erich.richter@gmail.com>
2020-07-03 16:53:22 -0700

    add styles

--- src/currency-exchanger.js
+++ src/currency-exchanger.js
@@ -2,12 +2,13 @@
 // exports!
 
 export class Exchange {
-  constructor(USD, AED, ARS, AUD, BGN) {
+  constructor(USD, AED, ARS, AUD, BGN, BRL) {
     this.USD = USD;
     this.AED = AED;
     this.ARS = ARS;
     this.AUD = AUD;
     this.BGN = BGN;
+    this.BRL = BRL;
   }
 
   usdToAed() {
@@ -15,6 +16,17 @@ export class Exchange {
   }
   usdToArs() {
     return parseFloat(this.USD * this.ARS).toFixed(2);
+  }
+  usdToAud() {
+    return parseFloat(this.USD * this.AUD).toFixed(2);
+  }
+
+  usdToBgn() {
+    return parseFloat(this.USD * this.BGN).toFixed(2);
+  }
+
+  usdToBrl() {
+    return parseFloat(this.USD * this.BRL).toFixed(2);
   }
 
 }
--- src/main.js
+++ src/main.js
@@ -8,36 +8,39 @@ import { Exchange } from './currency-exchanger';
 //business logic
 function doTheThing(resultWeReceive) {
   if (resultWeReceive) {
+    let BRL = parseFloat(resultWeReceive.conversion_rates.BRL).toFixed(2)
     let BGN = parseFloat(resultWeReceive.conversion_rates.BGN).toFixed(2)
     let AUD = parseFloat(resultWeReceive.conversion_rates.AUD).toFixed(2)
     let ARS = parseFloat(resultWeReceive.conversion_rates.ARS).toFixed(2)
     let AED = parseFloat(resultWeReceive.conversion_rates.AED).toFixed(2)
     let USD = parseFloat($("#usDollarAmount").val()).toFixed(2);
-    let exchange = new Exchange(USD, AED, ARS, AUD, BGN)
+    let exchange = new Exchange(USD, AED, ARS, AUD, BGN, BRL)
 
 
-if(parseInt($("#country").val()) === 1){
-    $("#results").text(exchange.usdToAed() + " United Arab Emirates Dirham(AED)")
-  
-
-  }else if (parseInt($("#country").val()) === 3){
-    $("#results").text(exchange.usdToArs() + " Australian Dollar(AUD)")
-  
-  }else if (Number.isInteger(parseInt($("#country").val())) === false){
-  $("#results").text("that currency doesn't exsist")
-
-}
+    if (parseInt($("#country").val()) === 1) {
+      $("#results").text($("#usDollarAmount").val() + " USD is equal to: " + exchange.usdToAed() + " United Arab Emirates Dirham(AED)")
+    } else if (parseInt($("#country").val()) === 2) {
+      $("#results").text($("#usDollarAmount").val() + " USD is equal to: " + exchange.usdToArs() + " Argentine Peso(ARS)")
+    } else if (parseInt($("#country").val()) === 3) {
+      $("#results").text($("#usDollarAmount").val() + " USD is equal to: " + exchange.usdToAud() + " Australian Dollar(AUD)")
+    } else if (parseInt($("#country").val()) === 4) {
+      $("#results").text($("#usDollarAmount").val() + " USD is equal to: " + exchange.usdToBgn() + " Bulgarian Lev(BGN)")
+    } else if (parseInt($("#country").val()) === 5) {
+      $("#results").text($("#usDollarAmount").val() + " USD is equal to: " + exchange.usdToBrl() + " Brazilian Real(BRL)")
+    } else if (Number.isInteger(parseInt($("#country").val())) === false) {
+      $("#results").text("that currency doesn't exsist")
+    }
     console.log(USD)
- console.log(AED)
-console.log(exchange)
-console.log($("#usDollarAmount").val())
-    
-console.log(parseInt($("#country").val()))
+    console.log(AED)
+    console.log(exchange)
+    console.log($("#usDollarAmount").val())
+
+    console.log(parseInt($("#country").val()))
 
 
-
+    //JSON.stringify(resultWeReceive.conversion_rates)
     $("#thingGoesHere").text(JSON.stringify(resultWeReceive.conversion_rates))
-   
+
     // $("#errorHere").html('');
   } else {
     // $("#errorHere").html(`${resultWeReceive}`);
@@ -49,26 +52,26 @@ console.log(parseInt($("#country").val()))
 // exchange = new Exchange()
 
 //user interface logic
-$(document).ready(function() {
-  
-  $("#getAPIbutton").click(function() {
+$(document).ready(function () {
+
+  $("#getAPIbutton").click(function () {
     console.log($("#usDollarAmount").val())
     fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`) //fetch is a shortcut; it creates a promise object that executes a GET API request on the url fed into it.
-      .then(function(responseJSON) {
+      .then(function (responseJSON) {
         if (!responseJSON.ok) {
           throw Error(responseJSON.statusText); //if response is woops, pass an error status down to catch and catch will do something with it
         }
         return responseJSON.json(); //if response is good, pass the response down to the next .then and .then will do something with it
       })
-      .catch(function(error) {  //only executes if response above was woops
+      .catch(function (error) {  //only executes if response above was woops
         return error;
       })
-      .then(function(messageFromPrevious) { //if .catch triggered, messageFromPrevious will be the  error message, if .catch did NOT trigger, messageFromPrevious will be the parsed JSON info 
+      .then(function (messageFromPrevious) { //if .catch triggered, messageFromPrevious will be the  error message, if .catch did NOT trigger, messageFromPrevious will be the parsed JSON info 
         doTheThing(messageFromPrevious);
-      
-       
+
+
       });
-      
-    });
-    
+
   });
+
+});
